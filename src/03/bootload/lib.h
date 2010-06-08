d55d2e000522ea908ecd32a9e9ea615101c2bf40
yku <kz2yz4@gmail.com>
2010-06-08 06:03:07 -0700

    f

---
+++ src/03/bootload/lib.h
@@ -0,0 +1,16 @@
+#ifndef _LIB_H_INCLUDED_
+#define _LIB_H_INCLUDED_
+
+int putc(unsigned char c);
+int puts(unsigned char *str);
+void *memset(void *b, int c, long len);
+void *memcpy(void *dst, const void *stc, long len);
+int  memcmp(const void *b1, const void *b2, long len);
+int strlen(const char *s);
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int len);
+int putxval(unsigned long value,int column);
+
+#endif
+
---
+++ src/03/bootload/main.c
@@ -0,0 +1,48 @@
+#include "defines.h"
+#include "serial.h"
+#include "lib.h"
+
+int global_data = 0x10;
+int global_bss;
+static int static_data = 0x20;
+static int static_bss;
+
+
+static void printval(void){
+  puts("global_data = "); putxval(global_data, 0); puts("\n");
+  puts("global_bss = "); putxval(global_bss, 0); puts("\n");
+  puts("static_data = "); putxval(static_data, 0); puts("\n");
+  puts("static_bss = "); putxval(static_bss, 0); puts("\n");
+}
+
+static int init(void){
+  extern int erodata, data_start, edata, bss_start, ebss;
+  
+  memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+  memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
+  serial_init(SERIAL_DEFAULT_DEVICE);
+
+  return 0;
+}
+
+int main(void){
+
+  init();
+  puts("Hello World!\n");
+
+  putxval(0x10,0);    puts("\n");
+  putxval(0xffff,0);  puts("\n");
+  printval();
+
+  puts("overwrite variables.\n");
+  global_data = 0x20;
+  global_bss = 0x30;
+  static_data = 0x40;
+  static_bss = 0x50;
+  printval();
+
+  while(1)
+    ;
+  return 0;
+}
