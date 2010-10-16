6674f676005ce71f07ace618aeaaa6e13915690e
yku <kz2yz4@gmail.com>
2010-10-17 00:37:20 +0900

    complete 8th step

--- src/08/os/main.c
+++ src/08/os/main.c
@@ -1,47 +1,21 @@
 #include "defines.h"
-#include "intr.h"
+#include "kozos.h"
 #include "interrupt.h"
-#include "serial.h"
 #include "lib.h"
 
-static void intr(softvec_type_t type, unsigned long sp)
+static int start_threads(int argc, char *argv[])
 {
-    int c;
-    static char buf[32];
-    static int len;
-
-    c = getc();
-
-    if (c != '\n') {
-        buf[len++] = c;
-    } else {
-        buf[len++] = '\0';
-        if ( !strncmp(buf, "echo",4)) {
-            puts(buf+4);
-            puts("\n");
-        }else{
-            puts("unknown. \n");
-        }
-        puts("> ");
-        len = 0;
-    }
+    kz_run(test08_1_main, "command", 0x100, 0, NULL);
+    return 0;
 }
 
 int main(void)
 {
-    INTR_DISABLE;
+    INTR_DISABLE; /* 割り込み無効 */
 
     puts("kozos boot succeed!\n");
-
-    softvec_setintr(SOFTVEC_TYPE_SERINTR, intr);
-    serial_intr_recv_enable(SERIAL_DEFAULT_DEVICE);
-
-    puts("> ");
-
-    INTR_ENABLE;
-    while(1){
-        asm volatile ("sleep");
-    }
+    
+    kz_start(start_threads, "start", 0x100, 0, NULL);
 
     return 0;
 }
